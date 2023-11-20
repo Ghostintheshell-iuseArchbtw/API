d617cb3a9ad0fe0686253bb3c92c47e1a2a7381c
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2023-11-20 17:35:20 +0000

    cool

---
+++ test_app.py
@@ -0,0 +1,60 @@
+import unittest
+import argparse
+import logging
+import os
+import time
+
+from app import deadmans_switch
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        # Set up any necessary variables or objects
+        self.file_paths = ['file1.txt', 'file2.txt']
+        self.key_file_path = 'key.txt'
+        self.decrypt = False
+        self.verbose = False
+        self.interval = 24
+        self.passphrase = '200OK'
+        self.generate_passphrase = False
+        self.passphrase_file = 'passphrase.txt'
+
+    def test_deadmans_switch_encrypt(self):
+        # Test the encryption functionality
+        args = argparse.Namespace(file_paths=self.file_paths, key_file_path=self.key_file_path,
+                                  decrypt=self.decrypt, verbose=self.verbose, interval=self.interval,
+                                  passphrase=self.passphrase, generate_passphrase=self.generate_passphrase,
+                                  passphrase_file=self.passphrase_file)
+
+        # Call the deadmans_switch function
+        deadmans_switch(args)
+
+        # TODO: Add assertions to check if the encryption operations were performed correctly
+
+    def test_deadmans_switch_decrypt(self):
+        # Test the decryption functionality
+        self.decrypt = True
+        args = argparse.Namespace(file_paths=self.file_paths, key_file_path=self.key_file_path,
+                                  decrypt=self.decrypt, verbose=self.verbose, interval=self.interval,
+                                  passphrase=self.passphrase, generate_passphrase=self.generate_passphrase,
+                                  passphrase_file=self.passphrase_file)
+
+        # Call the deadmans_switch function
+        deadmans_switch(args)
+
+        # TODO: Add assertions to check if the decryption operations were performed correctly
+
+    def test_deadmans_switch_generate_passphrase(self):
+        # Test the passphrase generation functionality
+        self.generate_passphrase = True
+        args = argparse.Namespace(file_paths=self.file_paths, key_file_path=self.key_file_path,
+                                  decrypt=self.decrypt, verbose=self.verbose, interval=self.interval,
+                                  passphrase=self.passphrase, generate_passphrase=self.generate_passphrase,
+                                  passphrase_file=self.passphrase_file)
+
+        # Call the deadmans_switch function
+        deadmans_switch(args)
+
+        # TODO: Add assertions to check if the passphrase was generated and saved correctly
+
+if __name__ == '__main__':
+    unittest.main()
