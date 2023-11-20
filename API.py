0af9d7d986b53ee75ea2b68e5cca1b45cb0d0e26
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2023-11-20 13:39:53 +0000

    wow

---
+++ API.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, request, jsonify
+import subprocess
+
+class GPT4API:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.app.route('/gpt4', methods=['POST'])(self.gpt4)
+
+    def run(self):
+        self.app.run()
+
+    def gpt4(self):
+        try:
+            data = request.get_json()
+            prompt = data['prompt']
+            
+            # Perform GPT 4.0 processing here
+            
+            response = {
+                'result': 'GPT 4.0 response'
+            }
+            return jsonify(response)
+        except Exception as e:
+            error_response = {
+                'error': str(e)
+            }
+            return jsonify(error_response), 500
+
+if __name__ == '__main__':
+    api = GPT4API()
+    api.run()
+
+    def install_dependencies():
+        try:
+            subprocess.check_call(['pip', 'install', 'flask'])
+            # Add any other dependencies you need to install here
+            # Example: subprocess.check_call(['pip', 'install', 'dependency_name'])
+            print("Dependencies installed successfully.")
+        except subprocess.CalledProcessError as e:
+            print("Failed to install dependencies:", e)
+
+    if __name__ == '__main__':
+        install_dependencies()
