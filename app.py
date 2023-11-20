43c826443865a5c94bd47ad0e6eacd09555f5ad3
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2023-11-20 17:06:12 +0000

    k

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
 import os
-import shutil
 import logging
-from cryptography.fernet import Fernet
+import getpass
+import argparse
+import datetime
+import shutil
+import random
+import string
+import subprocess
+import time
+import zipfile
+
+try:
+    from cryptography.fernet import Fernet
+    from cryptography.hazmat.primitives import hashes, padding
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+except ImportError:
+    subprocess.check_call(["python", "-m", "pip", "install", "cryptography"])
+
+    from cryptography.fernet import Fernet
+    from cryptography.hazmat.primitives import hashes, padding
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 def read_key_from_file(key_file_path):
     """
@@ -11,26 +31,99 @@ def read_key_from_file(key_file_path):
         key = key_file.read()
     return key
 
+def generate_random_key(key_file_path):
+    """
+    Generate a random encryption key and save it to a file.
+    """
+    key = Fernet.generate_key()
+    with open(key_file_path, 'wb') as key_file:
+        key_file.write(key)
+
+def generate_key(key_file_path):
+    """
+    Generate a new encryption key using a password and save it to a file.
+    """
+    salt = os.urandom(16)
+    password = getpass.getpass("Enter password: ").encode()
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = kdf.derive(password)
+    with open(key_file_path, 'wb') as key_file:
+        key_file.write(key)
+
+def generate_random_passphrase(passphrase_file_path):
+    """
+    Generate a random passphrase and save it to a file.
+    """
+    passphrase = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    with open(passphrase_file_path, 'w') as passphrase_file:
+        passphrase_file.write(passphrase)
+
 def encrypt_data(file_path, key):
     """
     Encrypt the data in the file using the provided key.
     """
+    chunk_size = 64 * 1024
+    cipher = Cipher(algorithms.AES(key), modes.CBC(os.urandom(16)))
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+
     with open(file_path, 'rb') as file:
-        data = file.read()
+        with open(file_path + '.enc', 'wb') as encrypted_file:
+            while True:
+                chunk = file.read(chunk_size)
+                if not chunk:
+                    break
+                padded_data = padder.update(chunk) + padder.finalize()
+                encrypted_chunk = encryptor.update(padded_data)
+                encrypted_file.write(encrypted_chunk)
 
-    cipher = Fernet(key)
-    encrypted_data = cipher.encrypt(data)
+def decrypt_data(file_path, key):
+    """
+    Decrypt the data in the file using the provided key.
+    """
+    chunk_size = 64 * 1024
+    cipher = Cipher(algorithms.AES(key), modes.CBC(os.urandom(16)))
+    decryptor = cipher.decryptor()
+    unpadder = padding.PKCS7(128).unpadder()
 
-    with open(file_path, 'wb') as file:
-        file.write(encrypted_data)
+    with open(file_path + '.enc', 'rb') as encrypted_file:
+        with open(file_path, 'wb') as decrypted_file:
+            while True:
+                chunk = encrypted_file.read(chunk_size)
+                if not chunk:
+                    break
+                decrypted_chunk = decryptor.update(chunk)
+                unpadded_data = unpadder.update(decrypted_chunk) + unpadder.finalize()
+                decrypted_file.write(unpadded_data)
+
+def compress_data(file_path):
+    """
+    Compress the data in the file using ZIP compression.
+    """
+    with zipfile.ZipFile(file_path + '.zip', 'w') as zip_file:
+        zip_file.write(file_path, os.path.basename(file_path))
+
+def decompress_data(file_path):
+    """
+    Decompress the data in the file using ZIP decompression.
+    """
+    with zipfile.ZipFile(file_path + '.zip', 'r') as zip_file:
+        zip_file.extractall(os.path.dirname(file_path))
 
 def shred_file(file_path):
     """
-    Overwrite the file with random data to securely delete its contents.
+    Overwrite the file with random data multiple times to securely delete its contents.
     """
-    file_size = os.path.getsize(file_path)
-    with open(file_path, 'wb') as file:
-        file.write(os.urandom(file_size))
+    with open(file_path, 'ab') as file:
+        file_size = os.path.getsize(file_path)
+        for _ in range(3):
+            file.seek(0)
+            file.write(os.urandom(file_size))
 
 def delete_file(file_path):
     """
@@ -38,74 +131,135 @@ def delete_file(file_path):
     """
     os.remove(file_path)
 
-def handle_nmap_script():
+def delete_directory(directory_path):
     """
-    Handle the Nmap script.
+    Delete the directory and its contents from the system.
     """
-    print("Nmap script detected!")
-    # Add your code here to handle the Nmap script
+    shutil.rmtree(directory_path)
 
-def handle_passphrase():
+def delete_empty_directories(directory_path):
+    """
+    Delete empty directories recursively.
+    """
+    for root, _, files in os.walk(directory_path, topdown=False):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            os.remove(file_path)
+        os.rmdir(root)
+
+def handle_passphrase(file_path, cipher, key_file_path, passphrase):
     """
     Handle the passphrase.
     """
-    print("Passphrase detected!")
-    # Add your code here to handle the passphrase
+    logging.info("Passphrase detected!")
+    attempts = 0
+    while attempts < 3:
+        user_input = getpass.getpass("Enter passphrase: ")
+        if user_input == passphrase:
+            encrypt_and_shred_self(file_path, cipher)
+            shred_file(key_file_path)
+            delete_file(key_file_path)
+            logging.info("System actions avoided for 48 hours. Thank you Dr. Falken.")
+            return
+        else:
+            attempts += 1
+            logging.info("Invalid passphrase. Please try again.")
+    current_time = datetime.datetime.now()
+    shockandpwn_time = datetime.datetime(2022, 1, 1)  # Replace with the desired shockandPWN time
+    time_left = shockandpwn_time - current_time
+    logging.info(f"Maximum passphrase attempts reached. Time left until shockandPWN: {time_left}")
 
 def encrypt_and_shred_self(file_path, cipher):
     """
     Encrypt and shred the script itself.
     """
-    with open(file_path, 'rb') as file:
-        data = file.read()
+    encrypt_data(file_path, cipher)
+    shred_file(file_path)
 
-    encrypted_data = cipher.encrypt(data)
+def encrypt_and_shred_file(file_path, cipher):
+    """
+    Encrypt and shred a file.
+    """
+    encrypt_data(file_path, cipher)
+    shred_file(file_path)
 
-    with open(file_path, 'wb') as file:
-        file.write(encrypted_data)
+def encrypt_and_shred_directory(directory_path, cipher):
+    """
+    Encrypt and shred all files in a directory.
+    """
+    for root, _, files in os.walk(directory_path):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            encrypt_and_shred_file(file_path, cipher)
 
-    file_size = os.path.getsize(file_path)
-    with open(file_path, 'wb') as file:
-        file.write(os.urandom(file_size))
+def deadmans_switch():
+    """
+    Main function to handle the deadman's switch.
 
-    new_file_path = file_path + '.shredded'
-    shutil.move(file_path, new_file_path)
+    This function parses command-line arguments, configures logging, reads the encryption key from a file,
+    creates a Fernet cipher object, and performs encryption, compression, and shredding operations based on the
+    provided arguments. It also checks for a passphrase at regular intervals and performs actions accordingly.
 
-def main():
-    # File path and key file path
-    file_path = '/workspaces/API/app.py'
-    key_file_path = '/workspaces/API/encryption_key.txt'
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Encrypt, compress, and shred files and directories.')
+    parser.add_argument('file_paths', type=str, nargs='+', help='Paths to the files or directories to encrypt, compress, and shred')
+    parser.add_argument('key_file_path', type=str, help='Path to the encryption key file')
+    parser.add_argument('--decrypt', action='store_true', help='Decrypt the files or directories instead of encrypting, compressing, and shredding')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose logging')
+    parser.add_argument('--interval', type=int, default=24, help='Interval in hours to check for the passphrase (default: 24)')
+    parser.add_argument('--passphrase', type=str, default='200OK', help='Passphrase to trigger the actions (default: 200OK)')
+    parser.add_argument('--generate-passphrase', action='store_true', help='Generate a random passphrase and save it to a file')
+    parser.add_argument('--passphrase-file', type=str, default='passphrase.txt', help='Path to the passphrase file (default: passphrase.txt)')
+    args = parser.parse_args()
+
+    # Configure logging
+    logging.basicConfig(level=logging.INFO if args.verbose else logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Read the encryption key from the file
-    encryption_key = read_key_from_file(key_file_path)
+    encryption_key = read_key_from_file(args.key_file_path)
 
     # Create a Fernet cipher object with the key
     cipher = Fernet(encryption_key)
 
-    # Continuously monitor for input
+    if args.generate_passphrase:
+        generate_random_passphrase(args.passphrase_file)
+        logging.info(f"Passphrase generated and saved to {args.passphrase_file}")
+        return
+
+    if args.decrypt:
+        for file_path in args.file_paths:
+            if os.path.isfile(file_path):
+                decrypt_data(file_path, cipher)
+                decompress_data(file_path)
+            elif os.path.isdir(file_path):
+                encrypt_and_shred_directory(file_path, cipher)
+                delete_empty_directories(file_path)
+            else:
+                logging.error(f"Invalid file or directory path: {file_path}")
+    else:
+        for file_path in args.file_paths:
+            if os.path.isfile(file_path):
+                compress_data(file_path)
+                encrypt_and_shred_file(file_path + '.zip', cipher)
+                shred_file(file_path + '.zip')
+            elif os.path.isdir(file_path):
+                encrypt_and_shred_directory(file_path, cipher)
+                delete_empty_directories(file_path)
+            else:
+                logging.error(f"Invalid file or directory path: {file_path}")
+
+    # Check for passphrase at regular intervals
     while True:
-        user_input = input("Enter Nmap script or passphrase: ")
-
-        if user_input == "nmap_script":
-            encrypt_and_shred_self(file_path, cipher)
-
-            with open(key_file_path, 'rb') as key_file:
-                key = key_file.read()
-            encrypted_key = cipher.encrypt(key)
-            with open(key_file_path, 'wb') as key_file:
-                key_file.write(encrypted_key)
-
-            shred_file(key_file_path)
-            delete_file(key_file_path)
-
-            handle_nmap_script()
-
-        elif user_input == "passphrase":
-            handle_passphrase()
-
-        else:
-            print("Invalid input. Please try again.")
+        for file_path in args.file_paths:
+            handle_passphrase(file_path, cipher, args.key_file_path, args.passphrase)
+        logging.info(f"Waiting for {args.interval} hours...")
+        time.sleep(args.interval * 3600)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    main()
+    deadmans_switch()
